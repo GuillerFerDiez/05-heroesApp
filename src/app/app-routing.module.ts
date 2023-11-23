af6484248255ead04704374598f1477ebaf2ffaa
GuillerFerDiez <79652538+GuillerFerDiez@users.noreply.github.com>
2023-11-23 11:56:36 +0100

    Final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ErrorPageComponent } from './shared/error-page/error-page.component';
+import { CommonModule } from '@angular/common';
+import { AuthGuard } from './auth/guards/auth.guard';
+//Importamos las dos funciones que creamos anteriormente.
+
 
 const routes: Routes = [
-  { path: 'heroes', loadChildren: () => import('./heroes/heroes.module').then(m =>m.HeroesModule)},
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m =>m.AuthModule)},
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  {
+    path: 'heroes', loadChildren: () => import('./heroes/heroes.module').then(m => m.HeroesModule),
+    canActivate: [AuthGuard],
+    canMatch: [AuthGuard],
+  },
   { path: '404', component: ErrorPageComponent },
-  { path: '**', redirectTo:'404' }
+  { path: '**', redirectTo: '404' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/heroes/pages/home/home.component.ts
+++ src/app/heroes/pages/home/home.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
 
+export class HomeComponent {
+  constructor(private router: Router, private authService: AuthService) { }
+
+  get auth() {
+    return this.authService.auth;
+  }
+
+  logout() {
+    this.router.navigate(['./auth']);
+  }
 }
